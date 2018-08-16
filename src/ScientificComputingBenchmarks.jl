c22743234735c16ab849a222899527b9a9ece387
Jonathan Bieler <jonathan.bieler@alumni.epfl.ch>
2018-08-16 21:34:18 +0200

    add R to travis

--- src/ScientificComputingBenchmarks.jl
+++ src/ScientificComputingBenchmarks.jl
@@ -3,7 +3,7 @@ module ScientificComputingBenchmarks
     function main()
         file = joinpath(@__DIR__,"Benchmarks","Statistics","Evaluating_pdf","Evaluating_pdf.py")
         tpy = parse(Float64, read(`python $file`,String) )
-        info(tpy)
+        @info tpy
         true
     end
 
